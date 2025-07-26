153857b6c30a58091da964f741516fe19c9b4ec4
Ángel Ruiz <97441137+Soul1227@users.noreply.github.com>
2025-07-26 18:28:28 +0200

    Ejemplo RestController

--- src/main/java/com/springproject/app1/controllers/EjemploController.java
+++ src/main/java/com/springproject/app1/controllers/EjemploController.java
@@ -1,14 +1,17 @@
 package com.springproject.app1.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class EjemploController {
 
     @GetMapping("/detalles_info")
-    public String getMethodName(){
+    public String info(Model model){
+        model.addAttribute("Titulo","Servidor en Linea");
+        model.addAttribute("Servidor", "infortamica");
+        model.addAttribute("IP","458.652.36.152");
         return "detalles_info";
     }
 }
---
+++ src/main/java/com/springproject/app1/controllers/EjemploRestController.java
@@ -0,0 +1,20 @@
+package com.springproject.app1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EjemploRestController {
+
+    @GetMapping("/info")
+    public Map<String, Object> info(){
+        Map<String, Object> map = new HashMap<>();
+        map.put("Titulo","Servidor en Linea");
+        map.put("Servidor", "infortamica");
+        map.put("IP","458.652.36.152");
+        return map;
+    }
+}
