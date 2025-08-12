c5b34e366ccba2602ecb4962b20e4cc12b62b66c
Ángel Ruiz <97441137+Soul1227@users.noreply.github.com>
2025-08-12 12:34:12 +0200

    Update EjemploRestController.java

--- src/main/java/com/springproject/app1/controllers/EjemploRestController.java
+++ src/main/java/com/springproject/app1/controllers/EjemploRestController.java
@@ -1,12 +1,14 @@
 package com.springproject.app1.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class EjemploRestController {
 
     @GetMapping("/info")
