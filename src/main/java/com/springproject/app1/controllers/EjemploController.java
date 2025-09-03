c1cf9f4b1cc0dd9cb1f49f4b5b3c5df8da93bb41
Ángel Ruiz <97441137+Soul1227@users.noreply.github.com>
2025-09-03 13:15:28 +0200

    Mostrar tabla de empleados haciendo uso de Thymeleaf y ModerlAttribute

--- src/main/java/com/springproject/app1/controllers/EjemploController.java
+++ src/main/java/com/springproject/app1/controllers/EjemploController.java
@@ -5,17 +5,30 @@ import com.springproject.app1.models.Empleados;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class EjemploController {
 
     @GetMapping("/detalles_info")
-    public String info(Model model){
+    public String info(Model model) {
 
         //Empleados empleado = new Empleados("Paco","Ruiz","Calle de la piruleta","Parado",40,659874123,1);
-        EmpleadosDTO empleado = new EmpleadosDTO("Ruben", "Pollito",25);
-        model.addAttribute("empleado",empleado);
+        EmpleadosDTO empleado = new EmpleadosDTO(null, "Pollito", 25);
+        model.addAttribute("empleado", empleado);
 
         return "detalles_info";
+    }
+
+    @ModelAttribute("Empleados")
+    public List<Empleados> listaEmpleados() {
+        return Arrays.asList(new Empleados("Paco", "Ruiz", "Calle de la piruleta", "Parado", 40, 478521653, 1),
+                new Empleados("Julia", "Benitez", "Calle Galleta", "Activa", 36, 874569123, 2),
+                new Empleados("Andrea", "Tamizo", "Calle Golosina", "Activa", 45, 852741963, 3),
+                new Empleados("Antonio", "Caprizo", "Calle Chocolate", "Parado", 34, 789456123, 4)
+        );
     }
 }
